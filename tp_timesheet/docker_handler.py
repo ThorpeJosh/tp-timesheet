79f1044d52b42fe8e864330e49fcd4fb60cb409f
chuntat <lamct1996@gmail.com>
2022-09-26 16:38:23 +0800

    add: time buffer for docker

--- tp_timesheet/docker_handler.py
+++ tp_timesheet/docker_handler.py
@@ -1,6 +1,7 @@
 """ Handler for running selenium and chrome backend """
 import platform
 import docker
+from time import sleep
 
 ARM_IMAGE = 'seleniarm/standalone-chromium:105.0'
 X86_IMAGE = 'selenium/standalone-chrome:105.0'
@@ -29,6 +30,14 @@ class DockerHandler:
     def run_container(self):
         """ Run the slenium image in a container """
         self.container = self.client.containers.run(self.image, auto_remove=True, detach=True, shm_size='2g', ports={'4444/tcp':4444})
+        # wait for container to run
+        timeout = 5
+        stop_time = 0.5
+        elasped_time = 0
+        while self.container.status != 'running' and elasped_time < timeout:
+            sleep(stop_time)
+            elasped_time += stop_time
+            continue
 
     def rm_container(self):
         """ Remove the container """
