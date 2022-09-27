cbede8fc73cd96d766812a6a883467a41579a60e
ThorpeJosh <josh@thorpe.engineering>
2022-09-27 17:02:01 +0800

    fix: account for selenium and chrome boot times

--- tp_timesheet/docker_handler.py
+++ tp_timesheet/docker_handler.py
@@ -4,6 +4,8 @@ import docker
 from time import sleep
 import http
 import urllib
+import urllib3
+from selenium import webdriver
 
 ARM_IMAGE = 'seleniarm/standalone-chromium:105.0'
 X86_IMAGE = 'selenium/standalone-chrome:105.0'
@@ -32,17 +34,33 @@ class DockerHandler:
     def run_container(self):
         """ Run the selenium image in a container """
         self.container = self.client.containers.run(self.image, auto_remove=True, detach=True, shm_size='2g', ports={'4444/tcp':4444})
+
+        # Ensure selenium api is responsive
+        for i in range(5):
+            try:
+                if urllib.request.urlopen("http://localhost:4444/").getcode() == 200:
+                    break
+            except (http.client.RemoteDisconnected, urllib.error.URLError):
+                print("#", i)
+                pass
+            if i == 4:
+                raise RuntimeError("Selenium Docker API did not return a 200 code within 5 seconds")
+            sleep(1)
+
+        # Ensure chrome driver is responsive
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-ssl-errors=yes')
+        options.add_argument('--ignore-certificate-errors')
         for i in range(10):
             try:
-                if urllib.request.urlopen("http://localhost:4444/").getcode() != 200:
-                    sleep(0.5)
-                else:
-                    print(urllib.request.urlopen("http://localhost:4444/").getcode())
+                with webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
+                    options=options) as browser:
                     break
-            except http.client.RemoteDisconnected as e:
-                continue
-            except urllib.error.URLError as e:
-                continue
+            except urllib3.exceptions.ProtocolError:
+                pass
+            if i == 4:
+                raise RuntimeError("Chrome Docker API did not respond within 5 seconds")
+            sleep(1)
 
 
     def rm_container(self):
