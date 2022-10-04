50ff281893a116ed521a77e9bf4e990952650d40
ThorpeJosh <josh@thorpe.engineering>
2022-10-04 09:56:22 +0800

    cicd: code formatting and lint error addressed

--- tp_timesheet/__init__.py
+++ tp_timesheet/__init__.py
@@ -1,1 +1,2 @@
-__version__='0.2.1'
+""" TP-Timesheet Module"""
+__version__ = "0.2.1"
--- tp_timesheet/__main__.py
+++ tp_timesheet/__main__.py
@@ -13,21 +13,50 @@ from workalendar.asia import Singapore
 
 def parse_args():
     """Parse arguments from the command line"""
-    parser = argparse.ArgumentParser(description='TP Automated Timesheet Submission - CLI Tool')
+    parser = argparse.ArgumentParser(
+        description="TP Automated Timesheet Submission - CLI Tool"
+    )
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-s', '--start', type=str,
-                        help="Normal mode: Indicating first date to submit a timesheet. Accepted arguments = ['dd/mm/yy' ,today]")
-    group.add_argument('-a', '--automate', type=str,
-                        help="Automate mode: Schedules the form submission to run automatically. Accepted arguments = [weekdays]")
-    parser.add_argument('-c', '--count', type=int, required=False, default=1,
-                        help="Number of weekdays to submit a timesheet for, use '5' on a monday to submit for the entire week")
-    parser.add_argument('-n', '--notification', action='store_true',
-                        help='Notification feature, notification will be shown when the timesheet submission is done(for OSX only)')
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help='Verbose mode, prints logs and saves screenshots of the timesheet submission page to your desktop')
-    parser.add_argument('-d', '--dry-run', action='store_true',
-                        help='Dry run mode, runs through as per normal but will not click submit')
+    group.add_argument(
+        "-s",
+        "--start",
+        type=str,
+        help="Normal mode: Indicating first date to submit a timesheet. Accepted arguments = ['dd/mm/yy' ,today]",
+    )
+    group.add_argument(
+        "-a",
+        "--automate",
+        type=str,
+        help="Automate mode: Schedules the form submission to run automatically. Accepted arguments = [weekdays]",
+    )
+    parser.add_argument(
+        "-c",
+        "--count",
+        type=int,
+        required=False,
+        default=1,
+        help="Number of weekdays to submit a timesheet for, use '5' on a monday to submit for the entire week",
+    )
+    parser.add_argument(
+        "-n",
+        "--notification",
+        action="store_true",
+        help="Notification feature, notification will be shown when the timesheet submission is done(for OSX only)",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Verbose mode, prints logs and saves screenshots of the timesheet submission page to your desktop",
+    )
+    parser.add_argument(
+        "-d",
+        "--dry-run",
+        action="store_true",
+        help="Dry run mode, runs through as per normal but will not click submit",
+    )
     return parser.parse_args()
+
 
 def run():
     """Entry point"""
@@ -35,7 +64,7 @@ def run():
     config = Config()
     # Automate Mode
     if args.automate is not None:
-        valid_options = ['weekdays']
+        valid_options = ["weekdays"]
         if args.automate not in valid_options:
             print(f"'{args.automate}' is not a valid option for --automate mode")
             return
@@ -49,37 +78,54 @@ def run():
         start_date = datetime.today()
     else:
         start_date = datetime.strptime(args.start, "%d/%m/%Y")
-    dates = date_fn(start = start_date, count = args.count, cal = cal)
+    dates = date_fn(start=start_date, count=args.count, cal=cal)
     print(f"Date(s) (yyyy-mm-dd) to be submitted for {config.EMAIL}:")
-    string_list=['%s: %d hours' % (date, hours) for (date, hours) in dates]
+    string_list = ["%s: %d hours" % (date, hours) for (date, hours) in dates]
     print(string_list)
 
     docker_handler = DockerHandler()
     try:
         if args.verbose:
-            print('Pulling latest image')
+            print("Pulling latest image")
         docker_handler.pull_image()
         if args.verbose:
-            print('Launching docker container for selenium backend')
+            print("Launching docker container for selenium backend")
         docker_handler.run_container()
 
         for (date, hours) in dates:
-            submit_timesheet(config.URL, config.EMAIL, date, verbose=args.verbose, dry_run=args.dry_run, working_hours=hours)
+            submit_timesheet(
+                config.URL,
+                config.EMAIL,
+                date,
+                verbose=args.verbose,
+                dry_run=args.dry_run,
+                working_hours=hours,
+            )
 
         # Notification (OSX only)
-        if args.notification and sys.platform.lower() == 'darwin':
+        if args.notification and sys.platform.lower() == "darwin":
             if len(dates) == 1:
-                notification_text = f'Timesheet for {args.start.lower()} is successfully submitted.'
+                notification_text = (
+                    f"Timesheet for {args.start.lower()} is successfully submitted."
+                )
             else:
-                notification_text = 'Timesheets from {} to {} are successfully submitted.'.format(dates[0], dates[-1])
-            os.system("""osascript -e 'display notification "{}" with title "TP Timesheet"'""".format(notification_text))
+                notification_text = (
+                    "Timesheets from {} to {} are successfully submitted.".format(
+                        dates[0], dates[-1]
+                    )
+                )
+            os.system(
+                """osascript -e 'display notification "{}" with title "TP Timesheet"'""".format(
+                    notification_text
+                )
+            )
 
     finally:
         if args.verbose:
-            print('Cleaning up docker container')
+            print("Cleaning up docker container")
         if docker_handler.container is not None:
             docker_handler.rm_container()
 
-if __name__ == '__main__':
-    run()
 
+if __name__ == "__main__":
+    run()
