67dcaab555688ce9241c13435941416921f72301
ThorpeJosh <josh@thorpe.engineering>
2022-10-04 09:56:22 +0800

    cicd: code formatting and lint error addressed

--- tp_timesheet/dates.py
+++ tp_timesheet/dates.py
@@ -1,16 +1,17 @@
 from datetime import timedelta
 
+
 def date_fn(start, count, cal):
-   dates = []
-   for i in range(count):
-      day = start+timedelta(days=i)
-      day=day.date()
-      year=day.year
-      holidays = cal.holidays(year)
-      holidates = [date for (date, _) in holidays]
-      if day.isoweekday() < 6:
-         if day not in holidates:
-            dates.append((day, 8))
-         else:
-            dates.append((day, 0))
-   return dates
+    dates = []
+    for i in range(count):
+        day = start + timedelta(days=i)
+        day = day.date()
+        year = day.year
+        holidays = cal.holidays(year)
+        holidates = [date for (date, _) in holidays]
+        if day.isoweekday() < 6:
+            if day not in holidates:
+                dates.append((day, 8))
+            else:
+                dates.append((day, 0))
+    return dates
