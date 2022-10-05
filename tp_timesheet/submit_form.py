0c260440955c4ed58800f9ccffaf1472dbf907b5
Chang-Uk Shin <changuk.shin.1991@gmail.com>
2022-10-05 14:25:49 +0800

    linting(modify disable syntax of pylint, modify syntax of initiating selenium browser)

--- tp_timesheet/submit_form.py
+++ tp_timesheet/submit_form.py
@@ -7,8 +7,8 @@ from PIL import Image
 DESKTOP_PATH = os.path.join(os.path.join(os.path.expanduser("~")), "Desktop")
 
 
-# pylint: disable-next=invalid-name, too-many-arguments
 def submit_timesheet(URL, EMAIL, date, verbose=False, dry_run=False, working_hours=8):
+    # pylint: disable-next=invalid-name, too-many-arguments
     """submit tp timesheet through selenium webdriver"""
     if not isinstance(date, datetime.date):
         raise TypeError(
@@ -18,85 +18,80 @@ def submit_timesheet(URL, EMAIL, date, verbose=False, dry_run=False, working_hou
     options = webdriver.ChromeOptions()
     options.add_argument("--ignore-ssl-errors=yes")
     options.add_argument("--ignore-certificate-errors")
-    browser = webdriver.Remote(
+
+    with webdriver.Remote(
         command_executor="http://localhost:4444/wd/hub", options=options
-    )
+    ) as browser:
 
-    # use browser to access desired webpage
-    browser.get(URL)
-    browser.maximize_window()
-    # wait a bit for elements on webpage to fully load
-    browser.implicitly_wait(5)
+        # use browser to access desired webpage
+        browser.get(URL)
+        browser.maximize_window()
+        # wait a bit for elements on webpage to fully load
+        browser.implicitly_wait(5)
 
-    # find the email field element and fill your email
-    email_field = browser.find_element(
-        "xpath",
-        "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[1]/div/div[3]/div/div/input",
-    )
-    email_field.send_keys(EMAIL)
-
-    # find the date field and fill date
-    date_field = browser.find_element(
-        "xpath",
-        "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/div/div/input[1]",
-    )
-    date_field.send_keys(date.strftime("%m/%d/%Y"))
-
-    if verbose:
-        # Capture image of top half of submission
-        image_path = os.path.join(
-            DESKTOP_PATH, f"timesheet_top_{date.strftime('%d_%m_%Y')}.png"
-        )
-        print(f"Saving top of timesheet to: {image_path}")
-        browser.save_screenshot(image_path)
-        image = Image.open(image_path)
-        image.show()
-
-    # find and fill in live hours
-    live_hours = browser.find_element(
-        "xpath",
-        "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[3]/div/div[3]/div/div/input",
-    )
-    live_hours.send_keys(working_hours)
-
-    # find and fill in the rest of the hours
-    idle_hours = browser.find_element(
-        "xpath",
-        "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[4]/div/div[3]/div/div/input",
-    )
-    idle_hours.send_keys("0")
-    training_hours = browser.find_element(
-        "xpath",
-        "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[5]/div/div[3]/div/div/input",
-    )
-    training_hours.send_keys("0")
-    tool_issues = browser.find_element(
-        "xpath",
-        "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[6]/div/div[3]/div/div/input",
-    )
-    tool_issues.send_keys("0")
-
-    if not dry_run:
-        # find submit button and click submit
-        submit = browser.find_element(
+        # find the email field element and fill your email
+        email_field = browser.find_element(
             "xpath",
-            "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[3]/div[1]/button/div",
+            "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[1]/div/div[3]/div/div/input",
         )
-        submit.click()
-    browser.implicitly_wait(5)
+        email_field.send_keys(EMAIL)
 
-    if verbose:
-        # Capture image of top half of submission
-        image_path = os.path.join(
-            DESKTOP_PATH, f"timesheet_bottom_{date.strftime('%d_%m_%Y')}.png"
+        # find the date field and fill date
+        date_field = browser.find_element(
+            "xpath",
+            "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/div/div/input[1]",
         )
-        print(f"Saving bottom of timesheet to: {image_path}")
-        browser.save_screenshot(image_path)
-        image = Image.open(image_path)
-        image.show()
+        date_field.send_keys(date.strftime("%m/%d/%Y"))
 
-    # close the browser after submitting
-    browser.quit()
+        if verbose:
+            # Capture image of top half of submission
+            image_path = os.path.join(
+                DESKTOP_PATH, f"timesheet_top_{date.strftime('%d_%m_%Y')}.png"
+            )
+            print(f"Saving top of timesheet to: {image_path}")
+            browser.save_screenshot(image_path)
+            image = Image.open(image_path)
+            image.show()
 
+        # find and fill in live hours
+        live_hours = browser.find_element(
+            "xpath",
+            "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[3]/div/div[3]/div/div/input",
+        )
+        live_hours.send_keys(working_hours)
 
-# pylint: enable=invalid-name, too-many-arguments
+        # find and fill in the rest of the hours
+        idle_hours = browser.find_element(
+            "xpath",
+            "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[4]/div/div[3]/div/div/input",
+        )
+        idle_hours.send_keys("0")
+        training_hours = browser.find_element(
+            "xpath",
+            "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[5]/div/div[3]/div/div/input",
+        )
+        training_hours.send_keys("0")
+        tool_issues = browser.find_element(
+            "xpath",
+            "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[6]/div/div[3]/div/div/input",
+        )
+        tool_issues.send_keys("0")
+
+        if not dry_run:
+            # find submit button and click submit
+            submit = browser.find_element(
+                "xpath",
+                "/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[3]/div[1]/button/div",
+            )
+            submit.click()
+        browser.implicitly_wait(5)
+
+        if verbose:
+            # Capture image of top half of submission
+            image_path = os.path.join(
+                DESKTOP_PATH, f"timesheet_bottom_{date.strftime('%d_%m_%Y')}.png"
+            )
+            print(f"Saving bottom of timesheet to: {image_path}")
+            browser.save_screenshot(image_path)
+            image = Image.open(image_path)
+            image.show()
