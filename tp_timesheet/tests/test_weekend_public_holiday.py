5b7bbfcda758cfdae8d702f745123c521859ca3c
ThorpeJosh <josh@thorpe.engineering>
2022-11-11 15:58:03 +0800

    cicd: fix broken test

--- tp_timesheet/tests/test_weekend_public_holiday.py
+++ tp_timesheet/tests/test_weekend_public_holiday.py
@@ -1,5 +1,5 @@
 """Unit tests for the date parsing method"""
-from datetime import date, datetime
+from datetime import date
 from workalendar.asia import Singapore
 from tp_timesheet.date_utils import get_working_dates
 
@@ -8,16 +8,16 @@ cal = Singapore()
 # fmt: off
 TEST_CASES = [
     # pylint: disable=line-too-long
-    (datetime(2022, 1, 10), 1, [(date(2022, 1, 10), 8)]),  # weekday
-    (datetime(2022, 1, 10), 3, [(date(2022, 1, 10), 8), (date(2022, 1, 11), 8), (date(2022, 1, 12), 8)]),  # consecutive weekdays
-    (datetime(2022, 1, 1), 1, []),  # weekend
-    (datetime(2022, 1, 1), 2, []),  # consecutive weekends
-    (datetime(2022, 8, 9), 1, [(date(2022, 8, 9), 0)]),  # holiday
-    (datetime(2022, 2, 1), 2, [(date(2022, 2, 1), 0), (date(2022, 2, 2), 0)]),  # consecutive holidays
-    (datetime(2022, 2, 11), 4, [(date(2022, 2, 11), 8), (date(2022, 2, 14), 8)]),  # weekday wrapping weekend
-    (datetime(2022, 8, 8), 3, [(date(2022, 8, 8), 8), (date(2022, 8, 9), 0), (date(2022, 8, 10), 8)]),  # weekdays wrapping holiday
-    (datetime(2022, 4, 14), 5, [(date(2022, 4, 14), 8), (date(2022, 4, 15), 0), (date(2022, 4, 18), 8)]),  # weekday wrapping holiday and weekends
-    (datetime(2022, 4, 15), 2, [(date(2022, 4, 15), 0)]),  # holiday to weekend
+    (date(2022, 1, 10), 1, [(date(2022, 1, 10), 8)]),  # weekday
+    (date(2022, 1, 10), 3, [(date(2022, 1, 10), 8), (date(2022, 1, 11), 8), (date(2022, 1, 12), 8)]),  # consecutive weekdays
+    (date(2022, 1, 1), 1, []),  # weekend
+    (date(2022, 1, 1), 2, []),  # consecutive weekends
+    (date(2022, 8, 9), 1, [(date(2022, 8, 9), 0)]),  # holiday
+    (date(2022, 2, 1), 2, [(date(2022, 2, 1), 0), (date(2022, 2, 2), 0)]),  # consecutive holidays
+    (date(2022, 2, 11), 4, [(date(2022, 2, 11), 8), (date(2022, 2, 14), 8)]),  # weekday wrapping weekend
+    (date(2022, 8, 8), 3, [(date(2022, 8, 8), 8), (date(2022, 8, 9), 0), (date(2022, 8, 10), 8)]),  # weekdays wrapping holiday
+    (date(2022, 4, 14), 5, [(date(2022, 4, 14), 8), (date(2022, 4, 15), 0), (date(2022, 4, 18), 8)]),  # weekday wrapping holiday and weekends
+    (date(2022, 4, 15), 2, [(date(2022, 4, 15), 0)]),  # holiday to weekend
     # pylint: enable=line-too-long
 ]
 # fmt: on
