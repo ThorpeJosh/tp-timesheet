e4984d29f7d135d7f1fcb568b0618c98fbe7c9a0
ThorpeJosh <josh@thorpe.engineering>
2022-11-25 11:18:21 +0800

    cicd: update tests for new config options

--- tp_timesheet/tests/test_config.py
+++ tp_timesheet/tests/test_config.py
@@ -1,8 +1,10 @@
 """Unit tests for the config module"""
+import builtins
 import os
 import sys
 import configparser
 import pytest
+import mock
 from tp_timesheet.config import Config
 
 tests_path = os.path.dirname(os.path.abspath(__file__))
@@ -22,6 +24,7 @@ def fixture_create_tmp_config():
 [configuration]
 tp_email = fake@email.com
 tp_url = https://example.com/path
+clockify_api_key = 1234
     """
     with open(test_config_path, "w", encoding="utf8") as conf_file:
         conf_file.write(config_str)
@@ -29,7 +32,26 @@ tp_url = https://example.com/path
     os.remove(test_config_path)
 
 
-def test_config_upgrade_process(tmp_config):
+@pytest.fixture(name="tmp_v3_config")
+def fixture_create_v3_0_0_config():
+    """
+    Creates a tmp config file with parameters from <=v0.3.0.
+    It then cleans up the tmp file after the test has run
+    """
+    # Fake config loaded by pytest
+    test_config_path = Config.CONFIG_DIR.joinpath("tmp_pytest.conf")
+    config_str = """
+[configuration]
+tp_email = fake@email.com
+tp_url = https://example.com/path
+    """
+    with open(test_config_path, "w", encoding="utf8") as conf_file:
+        conf_file.write(config_str)
+    yield test_config_path
+    os.remove(test_config_path)
+
+
+def test_config_upgrade_process(tmp_v3_config):
     """
     test the config from <=0.3.0 (email and url only),
     upgrades succesfully to include new parameters
@@ -46,27 +68,30 @@ def test_config_upgrade_process(tmp_config):
         return input_config
 
     # Check config file exists already (created by fixture)
-    assert os.path.exists(tmp_config)
+    assert os.path.exists(tmp_v3_config)
 
     # Assert no config parameters exist in config prior to upgrade
-    config_text = read_config_as_text(tmp_config)
+    config_text = read_config_as_text(tmp_v3_config)
     for line in config_text:
         for parameter in new_parameters:
             assert parameter not in line
 
     # Test config file reads correctly and only <=0.3.0 parameters exist
-    config_dict = read_config_as_dict(tmp_config)
+    config_dict = read_config_as_dict(tmp_v3_config)
     assert config_dict.sections() == ["configuration"]
     assert list(config_dict["configuration"]) == ["tp_email", "tp_url"]
 
     # Initialize config which performs the upgrade
-    Config(config_filename=tmp_config)
+    with mock.patch.object(
+        builtins, "input", lambda _: new_parameters["clockify_api_key"]
+    ):
+        Config(config_filename=tmp_v3_config)
 
     # Test config file reads correctly and contains all the new parameters
-    config_dict = read_config_as_dict(tmp_config)
+    config_dict = read_config_as_dict(tmp_v3_config)
     assert config_dict.sections() == ["configuration"]
-    assert list(config_dict["configuration"]) == ["tp_email", "tp_url"] + list(
-        new_parameters.keys()
+    assert sorted(list(config_dict["configuration"])) == sorted(
+        ["tp_email", "tp_url"] + list(new_parameters.keys())
     )
     for key, item in new_parameters.items():
         assert config_dict.get("configuration", key) == item
