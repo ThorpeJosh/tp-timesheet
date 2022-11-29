c53f7690498d8c2c845479f2990a1164585ebd3d
siewyeng <chowsiewyeng@hotmail.com>
2022-11-29 16:49:51 +0800

    raise response and make workspace id method private

--- tp_timesheet/clockify_timesheet.py
+++ tp_timesheet/clockify_timesheet.py
@@ -15,7 +15,7 @@ class Clockify:
     def __init__(self, api_key):
         self.api_key = api_key
 
-        self.workspace_id, self.user_id = self.get_workspace_user_id()
+        self.workspace_id, self.user_id = self._get_workspace_user_id()
         self.project_id = None
         self.task_id = None
 
@@ -50,13 +50,15 @@ class Clockify:
             response.text,
         )
 
-    def get_workspace_user_id(self):
+    def _get_workspace_user_id(self):
         """Send request to get workspace id"""
         get_request = requests.get(
-            "https://api.clockify.me/api/v1/user",
+            self.api_base_endpoint,
             headers={"X-Api-Key": self.api_key},
             timeout=2,
         )
+        get_request.raise_for_status()
+        logger.debug("Response: %s", get_request.text)
         request_dict = json.loads(get_request.text)
         workspace_id = request_dict["activeWorkspace"]
         user_id = request_dict["id"]
